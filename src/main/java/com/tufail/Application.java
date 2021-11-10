eee349b26d6ac2b22bc7b4869cf12aa08d73bf58
Tufal001 <tufail.khan56@gmail.com>
2021-11-11 01:38:51 +0530

    added variable

--- src/main/java/com/tufail/Application.java
+++ src/main/java/com/tufail/Application.java
@@ -9,7 +9,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
-		
+		int i=10;
 		run.close();
 	}
 
